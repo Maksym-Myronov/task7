af2d09b4e437f3a7a97343c5305738923ae4c91a
Maksym <115893111+Maksym-Myronov@users.noreply.github.com>
2022-12-13 19:11:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let addMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo');
+
+    let todoList = [];
+
+    if(localStorage.getItem('todo')){
+        todoList = JSON.parse(localStorage.getItem('todo'));
+        displayMessages();
+    }
+
+addButton.addEventListener('click', function(){
+
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important:false
+    };
+
+    todoList.push(newTodo);
+    displayMessages();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+
+});
+
+function displayMessages(){
+    let displayMessage = ''
+    todoList.forEach(function(item, i){
+         displayMessage += `
+        <li>
+        <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ' '}>
+        <label for='item_${i}' class="${item.important ? 'important' : ''}" >${item.todo}</label>
+        </li>
+        `;
+        todo.innerHTML = displayMessage;
+    });
+
+}
+
+todo.addEventListener('change', function(event){
+    let idInput= event.target.getAttribute('id');
+    let forLabel = todo.querySelector('[for='+ idInput +']');
+    let valueLabel = forLabel.innerHTML;
+    console.log('valueLabel: ', valueLabel);
+
+    todoList.forEach(function(item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+
+        } 
+    });
+});
+
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault();
+    todoList.forEach(function(item){
+        if(item.todo === event.target.innerHTML){
+            item.important = !item.important;
+            displayMessages();
+            localStorage.setItem('todo', JSON.stringify(todoList));
+
+        }
+    });
+});
+
+function onPageLoaded() {
+    const clearButton = document.querySelector("button.clear");
+    clearButton.addEventListener("click", () => {
+        todo.innerHTML = "";
+        localStorage.removeItem('todo',todo.innerHTML);
+    });
+}
+document.addEventListener("DOMContentLoaded", onPageLoaded);
